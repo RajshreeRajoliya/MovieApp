8b8626bc4bcfdb18579afca3134d116bd488e252
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-04-14 00:08:37 +0530

    backendofmovie

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user-routes";
+import adminRouter from "./routes/admin-routes";
+import movieRouter from "./routes/movie-routes";
+import bookingsRouter from "./routes/booking-routes";
+import cors from "cors";
+dotenv.config();
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/movie", movieRouter);
+app.use("/booking", bookingsRouter);
+
+
+
+mongoose.connect(`mongodb://admin:${process.env.MONGODB_PASSWORD}@ac-ixtbdpt-shard-00-00.ahpnpso.mongodb.net:27017,ac-ixtbdpt-shard-00-01.ahpnpso.mongodb.net:27017,ac-ixtbdpt-shard-00-02.ahpnpso.mongodb.net:27017/?ssl=true&replicaSet=atlas-klwjl7-shard-0&authSource=admin&retryWrites=true&w=majority`)
+
+.then(()=>
+app.listen(8080 , () =>
+console.log("Connected To Database And Server Is Running")
+)
+).catch((err) =>console.log(err))
